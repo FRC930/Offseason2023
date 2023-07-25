8a1359373906e33972fa1427aebb786b92a1f392
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2023-07-25 17:30:58 -0500

    7/25/23 5:30 Changed PIDs on the Arm

--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
@@ -17,11 +17,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
 public class ManipulatorSubsystem extends SubsystemBase {
-    
-    private final ProfiledPIDController controller;
-    private final ArmFeedforward ff;
-    private double targetPosition;
-    private final ManipulatorIO m_io;
+        private final ManipulatorIO m_io;
 
     public static final double ROLLER_INTAKE_SPEED = 0.5; //TODO find value
     public static final double HOLD_SPEED = 0.1; //TODO find value
@@ -35,16 +31,6 @@ public class ManipulatorSubsystem extends SubsystemBase {
      * @param io The ArmIO, use IORobot if robot is real, otherwise use IOSim.
      */
     public ManipulatorSubsystem (ManipulatorIO io) {
-
-        // Sets up PID controller TODO: Change these values
-        // controller = new ProfiledPIDController(0.2, 0, 0, new Constraints(360, 720));
-        controller = new ProfiledPIDController(0, 0, 0.0, new Constraints(540, 720)); //0.2
-        controller.setTolerance(1, 1);
-        //controller.enableContinuousInput(0, 360); // commented out for substation want to go long way!!
-
-        // Sets up Feetforward TODO: Change these values
-        ff = new ArmFeedforward(0.0, 0.0, 0); //g 0.35
-
         m_io = io;
     }
 
@@ -59,14 +45,6 @@ public class ManipulatorSubsystem extends SubsystemBase {
             
             this.m_io.updateInputs();
         }
-    }
-
-    /**<h3>setPosition</h3>
-     * Moves the manipulator to the desired position, using voltage.
-     * @param target Desired manipulator position in degrees
-     */
-    public void setPosition(double target) {
-        targetPosition = target;
     }
 
     /**<h3>getRollerVoltage</h3>
@@ -88,22 +66,5 @@ public class ManipulatorSubsystem extends SubsystemBase {
     public void setRollerSpeed(double speed) {
         m_io.setRollerSpeed(speed);
         Logger.getInstance().recordOutput("RunManipulatorRollerCommand/ManipulatorSpeed", speed);
-    }
-
-    public Command setWristPositionCommand(double degrees) {
-        return new InstantCommand(() -> setPosition(degrees), this);
-    }
-
-    private boolean atSetPoint() {
-        return this.controller.atGoal();
-    }
-
-    public Command createWaitUntilAtAngleCommand() {
-        return Commands.waitUntil(() -> this.atSetPoint());
-    }
-
-    public Command waitUntilCurrentPast(double amps) { 
-        Debouncer debouncer = new Debouncer(.02); //Creates a debouncer to confirm amps are greater than value for .1 seconds
-        return Commands.waitUntil(() -> debouncer.calculate(this.getRollerCurrent() > amps));
     }
 }
